ca26e2be74604ed935fa0ca4377b91ae9124dd53
Gustavo Viegas <webviegas@gmail.com>
2017-06-21 00:01:20 -0300

    HTTPS

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,6 +3,6 @@
 module.exports = {
   DEV_PORT: 8888,
   NODE_ENV: '"production"',
-  API_URL: '"http://api.uebmg.org.br/api/v1/"',
-  IMG_URL: '"http://api.uebmg.org.br/"'
+  API_URL: '"https://api.uebmg.org.br/api/v1/"',
+  IMG_URL: '"https://api.uebmg.org.br/"'
 }
